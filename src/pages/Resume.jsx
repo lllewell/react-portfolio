47d61d0f3abeb62f0ef7227321c2164ffd5050a8
Alien <lmllewellyn@gmail.com>
2024-09-22 14:21:35 -0400

    replaced names of components per README, added Resume component

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,8 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume Page</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, quisquam ullam minus totam doloribus distinctio nostrum. Molestiae, recusandae, debitis perferendis omnis nam totam labore itaque voluptates explicabo, cumque excepturi et?</p>
+        </div>
+    );
+}
