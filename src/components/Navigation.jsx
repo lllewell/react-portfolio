07925087487e052cbfdc2e23b0f0b8599c2ba96c
Alien <lmllewellyn@gmail.com>
2024-09-22 20:17:03 -0400

    removed style CSS from Contact page, fixed Navigation to include Portfolio section

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -28,9 +28,9 @@ function NavTabs() {
       </li>
       <li className="nav-item">
         <Link
-          to="/Blog"
+          to="/Portfolio"
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
         >
           Porfolio
         </Link>
