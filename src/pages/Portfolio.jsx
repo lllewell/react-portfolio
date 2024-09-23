43c1149f1838d3aa042ae157c81699786866d2c8
Alien <lmllewellyn@gmail.com>
2024-09-23 12:25:45 -0400

    added photos and files to Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,25 +1,21 @@
 import employeeTracker from '../images/employee-tracker.png'
+import seoRefactor from '../images/html-seo-refactor.png'
+import jateTextEditor from '../images/jate.png'
+import svgGenerator from '../images/svg.png'
 
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio Page</h1>
-      <img style={{width: '35%'}} src={employeeTracker} alt="employee-tracker"/>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <img style={{ width: '35%' }} src={employeeTracker} alt="employee-tracker" />
+        </div>
+        <div className='col-12'>
+          <img style={{ width: '25%' }} src={seoRefactor} alt="html-seo-refactor" />
+        </div>
+        <img style={{ width: '35%' }} src={jateTextEditor} alt="jate-text-editor" />
+        <img style={{ width: '35%' }} src={svgGenerator} alt="svg-generator" />
+      </div>
     </div>
   );
 }
